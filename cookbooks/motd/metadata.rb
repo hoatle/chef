fa27acc3aaee55f784752897a987937e35d7a353
Hoat Le <hoatlevan@gmail.com>
2013-08-15 04:33:34 +0700

    Test

---
+++ cookbooks/motd/metadata.rb
@@ -0,0 +1,7 @@
+name             'motd'
+maintainer       'Teracy, Inc'
+maintainer_email '/Users/hoatle/.chef/checksums'
+license          ''
+description      'Installs/Configures motd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
